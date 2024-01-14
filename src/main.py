110549744c270bf3ad5a3ab06860afbb2c01f4ca
Paul Ormazabal <paulormazabal1@gmail.com>
2024-01-14 18:43:20 -0300

    Se agrega login

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@ def sumar(a,b):
     return a + b
 
 
+def validar_usuario(usuario, contraseña):
+        usuarios_validos = {"usuario1": "contraseña123", "usuario2": "clave456"}
+        return usuario in usuarios_validos and usuarios_validos[usuario] == contraseña
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,7 +1,6 @@
 import pytest
 from src.main import sumar
-
-
+from src.main import validar_usuario
 
 
 @pytest.mark.parametrize(
@@ -20,6 +19,19 @@ def test_suma(input_a,input_b,esperado):
 def test_sumar():
     assert sumar(6,6)==12
 
+
+
+def test_validar_usuario_correcto():
+    assert validar_usuario("usuario1", "contraseña123") is True
+
+def test_validar_usuario_incorrecto():
+    assert validar_usuario("usuario1", "clave456") is False
+
+def test_validar_usuario_no_existente():
+    assert validar_usuario("usuario_no_existente", "contraseña123") is False
+
+def test_validar_contraseña_incorrecta():
+    assert validar_usuario("usuario1", "contraseña_incorrecta") is False
 
 
 
