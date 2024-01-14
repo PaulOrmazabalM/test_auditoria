8ac040a772dd463034970f83a8f1017cc38212ba
Paul Ormazabal <paulormazabal1@gmail.com>
2024-01-14 18:41:49 -0300

    Prueba con Funciones

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -1,4 +1,22 @@
+import pytest
 from src.main import sumar
+
+
+
+
+@pytest.mark.parametrize(
+    "input_a,input_b,esperado",
+[
+    (2,3,5),
+    (5,5,10),
+    (3,3,6),
+    (sumar(4,2),6,12),
+]
+)
+
+def test_suma(input_a,input_b,esperado):
+    assert sumar(input_a,input_b)==esperado
+
 def test_sumar():
     assert sumar(6,6)==12
 
